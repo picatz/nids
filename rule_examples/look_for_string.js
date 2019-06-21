40fe60d306aeaed6c12358ae166537b18615b777
Kent 'picat' Gruber <kgruber1@emich.edu>
2019-06-21 09:50:27 -0400

    Create look_for_string.js

---
+++ rule_examples/look_for_string.js
@@ -0,0 +1,5 @@
+// looks for a string found in ICMP packets (at least on macOS)
+// will print out a human-readable version of each packet it sees
+if (containsStr("01234567")) {
+    console.log(prettyPacket())
+}
